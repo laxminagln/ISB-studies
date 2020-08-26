c52b5895a930166e8b5cfefebc9357271d55aeb7
nagln <laxminagln@gmail.com>
2020-08-26 15:34:49 +0530

    update

--- ReactJS/color-changer/src/Background.js
+++ ReactJS/color-changer/src/Background.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Background.css';
 import Colorpick from './Colorpick';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 
 class Background extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "white"
+        };
+    }
+    
+    onColorChange = (e) => {
+        this.setState({
+            color: e.target.value
+        });
+    };
+
     render = () => {
         return(
-            <div className="back_container" style={{ backgroundColor = this.state.color}}>
+            <div className="back_container" style={{ backgroundColor: this.state.color}}>
                 <Colorpick
                     onChange = {this.onColorChange}
                     currentColor = {this.state.color}
