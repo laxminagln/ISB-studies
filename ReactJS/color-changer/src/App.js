5488d6e628834429f643b82e91919d9458eaba63
nagln <laxminagln@gmail.com>
2020-08-26 14:26:45 +0530

    update

--- ReactJS/color-changer/src/App.js
+++ ReactJS/color-changer/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Background from './Background';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Background />
     </div>
   );
 }
---
+++ ReactJS/color-changer/src/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Background.css';
+
+class Background extends React.Component {
+    render = () => {
+        return(
+            <div className="">
+
+            </div>
+        )
+    }
+}
