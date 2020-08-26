a64438be98ede84dad61ba1f8afbc1d6a0c4b2dc
nagln <laxminagln@gmail.com>
2020-08-26 15:05:22 +0530

    update

--- ReactJS/color-changer/src/Background.js
+++ ReactJS/color-changer/src/Background.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Background.css';
+import Colorpick from './Colorpick';
 
 class Background extends React.Component {
     render = () => {
         return(
-            <div className="back_container">
-                <div className="color_box">
-                    <h4>Pick a Color</h4>
-                    <input type="color" />
-                    <p>Mycurrent value</p>
-                </div>
+            <div className="back_container" style={{ backgroundColor = this.state.color}}>
+                <Colorpick
+                    onChange = {this.onColorChange}
+                    currentColor = {this.state.color}
+                />
             </div>
         )
     }
