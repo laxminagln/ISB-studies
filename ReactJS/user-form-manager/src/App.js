46e8fdf24e3314765dbfeee1c27ea4c16d37dfea
nagln <laxminagln@gmail.com>
2020-08-27 14:33:50 +0530

    update

--- ReactJS/user-form-manager/src/App.js
+++ ReactJS/user-form-manager/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Userform from './UserForm';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      
+      <Userform />
     </div>
   );
 }
